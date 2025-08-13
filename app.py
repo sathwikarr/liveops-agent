1fdd8fbc8f38cfae733834fdc3c5708953c93248
Sathwik Arroju <sathwikarroju@Sathwiks-MacBook-Air.local>
2025-08-13 16:38:35 -0400

    Update LiveOps dashboard and agent modules

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from ui.login import login_page
+from agent.explain import explain_anomaly
+from agent.detect import read_latest_data, zscore_anomaly_detection
 
 st.set_page_config(page_title="LiveOps Agent")
 
